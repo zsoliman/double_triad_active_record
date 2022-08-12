6cd7366ef6e0ae4d73e0eaa69eed1569d8517973
Michael Law <rmdashrfv@hey.com>
2022-08-12 11:54:35 -0400

    Added SQLite3

--- game.rb
+++ game.rb
@@ -1,5 +1,19 @@
 # Players have a deck (a deck is an array of cards)
 # cards have stats
+require 'securerandom'
+require 'sqlite3'
+
+db = SQLite3::Database.new("game.db")
+
+# Create a table
+rows = db.execute <<-SQL
+  create table numbers (
+    name varchar(30),
+    val int
+  );
+SQL
+
+p rows
 
 class Player
   attr_accessor :name, :email, :password, :deck
@@ -11,6 +25,8 @@ class Player
   end
 end
 
+# In order to save this information we would need to create a Cards table in a database
+# when we create a card object, we would store that card object in the database
 class Card
   attr_accessor :name, :top, :left, :bottom, :right, :element, :img
   def initialize(name, top, left, bottom, right, element=nil, img=nil)
@@ -21,5 +37,7 @@ class Card
     @right = right
     @element = element
     @img = img
+    @card_id = SecureRandom.uuid
+    @owner = owner
   end
 end
